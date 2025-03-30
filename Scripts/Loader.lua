05d2250c85b15204fbcffaf6f9b8b7f744357639
Kaguya11 <106523037+Kaguya11@users.noreply.github.com>
2025-03-31 02:24:49 +0500

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -1,1 +1,32 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua'))()
+
+local PlaceId = game.PlaceId
+local lplr = game:GetService("Players").LocalPlayer
+local market = game:GetService("MarketplaceService")
+local info = market:GetProductInfo(PlaceId)
+local games = {
+    [70876832253163] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/deadrails.lua', -- dead rails
+    [116495829188952] = 'https://pastebin.com/raw/1bxevcFK'
+}
+
+if identifyexecutor() then
+    local executor = identifyexecutor()
+    if executor == "Solara" or executor == "Xeno" or executor == "Nezur" then
+        lib:Notify('Kagu Hub work bad with xeno, solara, jjsploit and other free exec')
+        setclipboard("https://discord.gg/AkWWsyw2eG")
+    end
+end
+
+if games[PlaceId] then 
+    lib:Notify('Supported game loading: ' .. info.Name)
+    loadstring(game:HttpGet(games[PlaceId]))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaguya11/KaguHubRework/main/KaguHub"))() 
+    -- t.me/kaguyaxscript | https://discord.gg/AkWWsyw2eG
+    lib:Notify('Game not supported: ' .. info.Name .. ' if you wanna support this game join the discord copied to your clipboard')
+    setclipboard("https://discord.gg/AkWWsyw2eG")
+end
