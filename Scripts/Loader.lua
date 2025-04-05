d963bcf4a0d005b9604bf74305910a6ceaeeb409
Kaguya11 <106523037+Kaguya11@users.noreply.github.com>
2025-04-05 16:51:49 +0500

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -9,9 +9,10 @@ local lplr = game:GetService("Players").LocalPlayer
 local market = game:GetService("MarketplaceService")
 local info = market:GetProductInfo(PlaceId)
 local games = {
-    [70876832253163] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/DeadRails.lua', -- dead rails
-    [116495829188952] = 'https://pastebin.com/raw/1bxevcFK',
-    [4639625707] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/WarTycoon.lua'
+    [70876832253163] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/DeadRails.lua', -- Dead rails
+    [116495829188952] = 'https://pastebin.com/raw/1bxevcFK', -- Lobby Dead Rails
+    [4639625707] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/WarTycoon.lua', -- War Tycoon
+    [17625359962] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/Rivals.lua' -- Rivals
 }
 
 if identifyexecutor() then
