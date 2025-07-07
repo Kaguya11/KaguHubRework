932cb56a357fbbd510c321b950ac5f57f6dd9a44
Kaguyaa1 <106523037+Kaguya11@users.noreply.github.com>
2025-07-08 03:35:43 +0500

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -9,19 +9,23 @@ local lplr = game:GetService("Players").LocalPlayer
 local market = game:GetService("MarketplaceService")
 local info = market:GetProductInfo(PlaceId)
 local games = {
+    [117398147513099] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/Rivals.lua', -- rivals other
     [4639625707] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/WarTycoon.lua', -- War Tycoon
     [17625359962] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/Rivals.lua', -- Rivals
     [4543144283] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/Mega%20Noob%20Simulator.lua', -- Mega Noob Simulator
     [11276071411] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/BeNPCorDie.lua', -- Be NPC or Die
     [14943334555] = 'https://raw.githubusercontent.com/Kaguya11/KaguHubRework/refs/heads/main/Scripts/BikeOfHell.lua' -- Bike of Hell
 }
-
+setclipboard(PlaceId) 
 if games[PlaceId] then 
     lib:Notify('Supported game loading: ' .. info.Name)
     loadstring(game:HttpGet(games[PlaceId]))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaguya11/KaguHubRework/main/KaguHub"))() 
-    -- t.me/kaguyaxscript | https://discord.gg/AkWWsyw2eG
-    lib:Notify('Game not supported: ' .. info.Name .. ' if you wanna support this game join the discord copied to your clipboard')
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaguya11/KaguHubRework/main/KaguHub.lua"))()
     setclipboard("https://discord.gg/AkWWsyw2eG")
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Kagu Hub",
+        Text = "Game not supported: " .. info.Name,
+        Duration = 5
+    })
 end
